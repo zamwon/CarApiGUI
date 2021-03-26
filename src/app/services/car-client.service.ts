48b93fc768ac25ed71b39add803520d1425a8b08
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-03-26 20:23:26 +0100

    [change] merge

--- src/app/services/car-client.service.ts
+++ src/app/services/car-client.service.ts
@@ -3,7 +3,6 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -15,11 +14,13 @@ export class CarClientService {
   getCars(): Observable<Car[]> {
     return this.httpClient.get<Car[]>('http://localhost:8080/cars');
   }
+
   addCar(car: Car): Observable<Car> {
     return this.httpClient.post<Car>('http://localhost:8080/cars', car);
   }
 }
-export interface Car{
+
+export interface Car {
   carId: number;
   mark: string;
   model: string;
