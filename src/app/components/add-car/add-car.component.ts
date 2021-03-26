e198a7b73d2aeb2fa161d42f513784e88104e3c0
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-03-26 21:16:19 +0100

    [fix] create Car object

--- src/app/components/add-car/add-car.component.ts
+++ src/app/components/add-car/add-car.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Car, CarClientService} from '../../services/car-client.service';
 
 
-
 @Component({
   selector: 'app-add-car',
   templateUrl: './add-car.component.html',
@@ -10,12 +9,6 @@ import {Car, CarClientService} from '../../services/car-client.service';
 })
 export class AddCarComponent implements OnInit {
   car: Car;
-  cars: Car[];
-  carId: number;
-  mark: string;
-  model: string;
-  color: string;
-  productionYear: number;
 
   constructor(public carClientService: CarClientService) {
   }
@@ -24,11 +17,25 @@ export class AddCarComponent implements OnInit {
   }
 
   createCar(carId: number, mark: string, model: string, color: string, productionYear: number): Car {
-    this.carId = carId,
-      this.mark = mark,
-      this.model = model,
-      this.color = color,
-      this.productionYear = productionYear;
+    // tslint:disable-next-line:new-parens
+    this.car = new class implements Car {
+      carId: number;
+      color: string;
+      mark: string;
+      model: string;
+      productionYear: number;
+    };
+    this.car.carId = carId;
+    this.car.mark = mark;
+    this.car.model = model;
+    this.car.color = color;
+    this.car.productionYear = productionYear;
+    console.log(this.car.carId);
+    console.log(this.car.model);
+    console.log(this.car.mark);
+    console.log(this.car.color);
+    console.log(this.car.productionYear);
+    console.log(this.car);
     return this.car;
   }
 }
